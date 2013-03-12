fd7477f47fa933fe4df6c77d771f92ddd3b68519
Austin Benson <arbenson@gmail.com>
2013-03-12 08:27:34 -0700

    estm working

--- estm.py
+++ estm.py
@@ -20,6 +20,7 @@ class ESTMDataGen:
     
     return len(self.cumw) - 1
 
+  # TODO: consolidate with index_sample()
   def doc_sample(self, j):
     unif_samp = rand.uniform(0, 1)
     for i, s in enumerate(self.cumA[:,j]):
@@ -46,19 +47,43 @@ class ESTMDataGen:
   def gen_tensor(self, n):
     return self.inner_gen_tensor(n)
 
-w = [0.7, 0.3]
-A = np.array([[1., 0.], [0., 1.], [0., 0.], [0., 0.]])
-g = ESTMDataGen(w, A)
+if __name__ == "__main__":
+  w = [0.5, 0.3, 0.2]
+  A = np.array([[0.6, 0.3, 0.2], [0.3, 0.2, 0.5], [0.2, 0.5, 0.3]])
+  #print "original weights:\n", w
+  #print "original distribs:\n", A
 
-t0 = time.time()
-M2, M3 = g.gen_tensor(10000)
-print M2
-print M3
-print np.linalg.eig(M2)
-evecs, evals = la.eig(M3, 25, 200)
+  eval_errs = []
+  evec_errs = []
+  conds = []
 
-# normalized eigenvalues
-print evals[0:2] / sum(evals[0:2])
-for k in xrange(2):
-  print evecs[k, :].T / np.linalg.norm(evecs[k, :], 1)
-print "time: ", time.time() - t0
+  N = 100000
+
+  for eps in [1e-1, 1 - 1e-2, 1 - 1e-4, 1 - 1e-6, 1 - 1e-8, 1 - 1e-10, 1 - 1e-12]:
+    B = np.zeros((3, 3))
+    B[:, 0:2] += A[:, 0:2]
+    B[:, 2] += eps * A[:, 1] + (1 - eps) * A[:, 2]
+    conds.append(np.linalg.cond(B, 2))
+
+    num_trials = 12
+    eval_trial_err = np.zeros(len(w))
+    evec_trial_err = np.zeros(B.shape[1])
+
+    for trial in xrange(num_trials):
+      g = ESTMDataGen(w, B)
+      M2, M3 = g.gen_tensor(N)
+      W, X3 = la.whiten(M2, M3)
+      evals, evecs = la.reconstruct(W, X3)
+
+      eval_trial_err += np.array([abs(w[i] - evals[i]) / abs(w[i]) for i in xrange(len(w))])
+      evec_trial_err += np.array([np.linalg.norm(evecs[i,:] - B[:, i], 2) / np.linalg.norm(evecs[i,:], 2) \
+                                  for i in xrange(B.shape[1])])
+
+    print "updating..."
+    eval_errs.append(list(eval_trial_err / num_trials))
+    evec_errs.append(list(evec_trial_err / num_trials))
+
+  print conds
+  print eval_errs
+  print evec_errs
+
